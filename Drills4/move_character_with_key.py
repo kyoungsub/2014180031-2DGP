7233035ce5000e04d97d2f5ffb99bf4a0b3d361f
Gyeongseop Lee <deathkill200@gmail.com>
2019-10-07 17:27:26 +0900

    Drill4 완료
    2014180031 이경섭
    https://youtu.be/6lqcusyizFo

--- Drills4/move_character_with_key.py
+++ Drills4/move_character_with_key.py
@@ -17,45 +17,45 @@ def handle_events():
                 animation_key=1
             elif event.key == SDLK_LEFT:
                 dir -= 1
-                animation_key =1
+                animation_key =0
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
                 dir -= 1
-                animation_key =0
+                animation_key =3
             elif event.key == SDLK_LEFT:
                 dir += 1
-                animation_key=0
+                animation_key=2
 
 open_canvas()
 grass = load_image('grass.png')
-character= load_image('run_animation.png')
-idle = load_image('character.png')
+character= load_image('animation_sheet.png')
 x = 800 //2
 frame = 0
 dir = 0
-animation_key = 0
+animation_key = 3
 running = True
 
 while running:
     clear_canvas()
     grass.draw(400, 30)
-    if animation_key == 0:
-        idle.draw_now(x,90)
-    else:
-        character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    character.clip_draw(frame * 100, 100 * animation_key, 100, 100, x, 90)        
     update_canvas()
     handle_events()
     frame = (frame + 1) % 8
     if x>=800 and dir == 1:
         x = 800
-        animation_key = 0
+        animation_key = 3
     elif x<=0 and dir == -1:
         x=0
-        animation_key = 0
-    else:
+        animation_key = 2
+    elif dir == 1:
         x += dir * 5
+        animation_key = 1
+    elif dir == -1:
+        x += dir *5
+        animation_key = 0
     delay(0.01)
 
 
