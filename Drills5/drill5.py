0c5ae1d905168b4d8d765212fbcc96ff2d2f67dc
Gyeongseop Lee <deathkill200@gmail.com>
2019-10-09 16:37:48 +0900

    버그가 좀있으나 움직이긴함

--- Drills5/drill5.py
+++ Drills5/drill5.py
@@ -4,15 +4,19 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 def handle_events():
     global running
-    global x, y
+    global c_x, c_y
+    global click_x, click_y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, KPU_HEIGHT - 1 - event.y
+            c_x, c_y = event.x, KPU_HEIGHT - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            click_x, click_y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+ 
 
 open_canvas(KPU_WIDTH,KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
@@ -21,7 +25,9 @@ cursor_arrow = load_image('hand_arrow.png')
 
 
 running = True
-x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+c_x, c_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+p_x,p_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+click_x, click_y = 0, 0
 frame = 0
 hide_cursor()
 
@@ -31,7 +37,14 @@ hide_cursor()
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    cursor_arrow.draw(x, y)
+    cursor_arrow.draw(c_x, c_y)
+
+    for i in range(0,100+1,2):
+        t= i/100
+        p_x = (1-t)* p_x + t * click_x
+        p_y = (1-t) * p_y + t* click_y
+        character.clip_draw(frame * 100, 100, 100, 100, p_x, p_y)
+    
     update_canvas()
 
     handle_events()
