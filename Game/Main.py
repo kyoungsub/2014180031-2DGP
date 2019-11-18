0f23e91f59606270996cf6c3a67cf2b63b2dda03
Gyeongseop Lee <deathkill200@gmail.com>
2019-11-18 12:33:23 +0900

    slash_and game_world

--- Game/Main.py
+++ Game/Main.py
@@ -4,10 +4,12 @@ import os
 
 from pico2d import *
 import game_framework
+import game_world
 
 from Player import Player
 from Land import Land
 from Background import Background
+from Slash import Slash
 
 
 name = "MainState"
@@ -15,20 +17,29 @@ name = "MainState"
 player = None
 land = None
 background = None
+slash = []
 
+def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+    return True
 
 def enter():
-    global player, land, background
+    global player, land, background, slash
     player = Player()
+    game_world.add_object(player, 1)
     land = Land()
+    game_world.add_object(land, 0)
     background = Background()
+    game_world.add_object(player, 0)
 
 
 def exit():
-    global player, land, background
-    del player
-    del land
-    del background
+    game_world.clear()
 
 
 def pause():
@@ -54,15 +65,13 @@ def handle_events():
 
 
 def update():
-    player.update()
-    land.update()
-    background.update()
+    for game_object in game_world.all_objects():
+        game_object.update()
 
 
 def draw():
     clear_canvas()
-    background.draw()
-    land.draw()
-    player.draw()
+    for game_object in game_world.all_objects():
+        game_object.draw()
     update_canvas()
 
